dbd32927713bf3d9ad679fb99a5b3f922d8d4c4a
Evjenth <anstroen@gmail.com>
2020-10-03 13:32:46 +0200

    add dank trees

---
+++ Assets/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public TreeState treeState = TreeState.Sapling;
+    public Sprite[] treeSprites;
+
+    private SpriteRenderer _spriteRenderer;
+
+    
+    
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = treeSprites[0];
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Time.realtimeSinceStartup > 10 && treeState == TreeState.Sapling)
+        {
+            _spriteRenderer.sprite = treeSprites[1];
+            treeState = TreeState.Grown;
+        }
+    }
+}
+
+public enum TreeState
+{
+    Sapling, Grown, ChoppedDown
+}
