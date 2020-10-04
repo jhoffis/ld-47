95fad71e79513a9296504f86e5afcbc0828d78bd
jonathan hofslundsengen <jonahoffis@gmail.com>
2020-10-04 19:30:19 +0200

    kan gå oppå stubbe nå

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Random = System.Random;
 
 public class GameController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class GameController : MonoBehaviour
     public PlayerController playerController;
 
     private long _nextTreeTime;
+    private Random ran = new Random();
     private void Awake()
     {
         if (Instance == null)
@@ -34,10 +36,12 @@ public class GameController : MonoBehaviour
         long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
         if (_nextTreeTime > now) return;
 
-        _nextTreeTime = now + 1000;
+        _nextTreeTime = now + 5000 + ran.Next(5000);
         
-        Instantiate (Resources.Load ("Prefabs/Tree") as GameObject);
+        var tree = Instantiate (Resources.Load ("Prefabs/Tree") as GameObject);
         // plasser på en ledig plass tilfeldig.
-        
+        int size = 32;
+        tree.transform.position = new Vector3(ran.Next(size) - size / 2, ran.Next(size) - size / 2);
+        Debug.Log("Tree spawned at " + tree.transform.position.ToString());
     }
 }
