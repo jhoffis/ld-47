0d1f786be96fc6d1ec8d7aa0887522dcb45e74ed
ffauskanger <33883489+ffauskanger@users.noreply.github.com>
2020-10-05 20:59:28 +0200

    Upgrademenu

---
+++ Assets/Scripts/UpgradeMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UpgradeMenu : MonoBehaviour
+{
+
+    private GameObject upgradeMenuInterface;
+
+    void Awake()
+    {
+        upgradeMenuInterface = transform.parent.gameObject;
+        upgradeMenuInterface.SetActive(false);
+
+        //var upgradeButton = Resources.Load<GameObject>("Prefabs/BuildingButton");
+        //var upgradeButtons = new GameObject[BuildingObject.BuildingNames.Length];
+
+        //for (int i = 0; i < buildingButtons.Length; i++)
+        //{
+        //    var pos = new Vector2(transform.position.x, 200 + transform.position.y + (i * (-40)));
+        //    buildingButtons[i] = Instantiate(buildingButton, pos, Quaternion.identity);
+        //    buildingButtons[i].transform.parent = transform;
+        //    buildingButtons[i].GetComponent<SelectBuilding>().BuildingType = i;
+        //    buildingButtons[i].GetComponentInChildren<TMP_Text>().text = BuildingObject.BuildingNames[i];
+        //}
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/UpgradePanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePanel : MonoBehaviour
+{
+    private PlayerController playerController;
+    private GameObject button;
+    private GameObject price;
+    private GameObject upgrade;
+
+    void Awake()
+    {
+        playerController = GameController.Instance.playerController;
+        button = transform.GetChild(0).gameObject;
+        price = transform.GetChild(1).gameObject;
+        upgrade = transform.GetChild(2).gameObject;
+    }
+
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
